72f47e54b88ef7bcefbf904752fc0a16c2d528fd
Lamontsm <steven@lamonaco.com>
2013-11-08 16:18:38 -0800

    New additions from 8 Nov
    put in win/loss outcomes and multiple rolls.

--- Casino/Javascript/CasinoCraps.js
+++ Casino/Javascript/CasinoCraps.js
@@ -3,40 +3,54 @@
  */
 
 /* First let's roll the dice */
-var dice1;
-var dice2;
+var dice1, dice2;
 document.writeln("<image src='Graphics/pair-of-dice.jpg'></image>");
 document.write("<p></p>");
 dice_roll();
 var total=dice1+dice2;
+var mark=total;
+
+var play_on=true;
 document.write("<p></p>");
 document.writeln("You rolled a..."+total);
 document.write("<p></p>");
 
 /* Let's see if you won on the first roll */
 
-If ((total==7) || (total==11)) {
+if ((total==7) || (total==11)) {
     document.write("You win!");
-    play_on=false
+    play_on=false;
 }
-If ((total==2) || (total==12)) {
+if ((total==2) || (total==12)) {
     document.write("You crapped out!");
     play_on=false;
 }
-/* Then let's keep rolling!
-While (var play_on=true) {
+/* Then let's keep rolling!  */
+if (play_on==true){
+    document.write("Let's keep rolling...");
+}
+while (play_on==true) {
+    dice_roll();
+    total=dice1+dice2;
+    document.write("<p></p>");
+    document.writeln("You rolled a..."+total);
 
-
-
-} */
+    if (total==7) {
+        document.write("You lose!");
+        play_on=false;
+    }
+    if (total==mark) {
+        document.write("You win!");
+        play_on=false;
+    }
+}
 
 
 function dice_roll()
    {
    /* this rolls the two dice and prints the outcome*/
-    dice1 = Math.round(6*Math.random());
-    dice2=Math.round(6*Math.random());
+    dice1 = Math.round(.4999+6*Math.random());
+    dice2=Math.round(.4999+6*Math.random());
+   document.write("<p></p>");
    document.write("Die 1: "+dice1+"   Die 2: "+dice2);
-
-   return;
    }
